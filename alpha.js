7d3608ec558168f92d0ffe2ee9fd334d7852b9fb
F4R0UQ-2005 <surveyeah300@gmail.com>
2025-01-03 15:40:54 +0100

    Added unfollow button

--- alpha.js
+++ alpha.js
@@ -1,16 +1,17 @@
-document.getElementById("follow-button").addEventListener("click", function () {
-    const button = this;
-    const textSpan = document.querySelector("#follow-text");
-    const iconSpan = document.querySelector("#follow-icon");
+//Follow button//
+document.querySelectorAll('.follow-button').forEach(button => {
+  button.addEventListener('click', () => {
+    const icon = button.querySelector('.follow-icon');
+    const text = button.querySelector('.follow-text');
 
-    
-    if (button.classList.contains("unfollow")) {
-      button.classList.remove("unfollow");
-      textSpan.textContent = "Follow";
-      iconSpan.textContent = "➕"; 
+    if (button.classList.contains('following')) {
+      button.classList.remove('following');
+      text.textContent = 'Follow';
+      icon.textContent = '➕';
     } else {
-      button.classList.add("unfollow");
-      textSpan.textContent = "Unfollow";
-      iconSpan.textContent = "➖";
+      button.classList.add('following');
+      text.textContent = 'Following';
+      icon.textContent = '✔';
     }
   });
+});
